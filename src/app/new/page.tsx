d94bce9c68ec212671cd68d22000911c65048a8c
Reza Zahedi <r.zahedi@gmail.com>
2023-05-26 14:35:19 -0700

    Feat: Habits list added, New habit btn and page created

---
+++ src/app/new/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+	return (
+		<h1>New Habit</h1>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+import Link from 'next/link'
+import { prisma } from '@/db'
 
-export default function Home() {
-  return (
-    <h1>Hi</h1>
-  )
+function getHabits () {
+	return prisma.habits.findMany()
+}
+
+export default async function Home() {
+	
+	const habits = await getHabits()
+
+	return (
+		<>
+			<header className="flex justify-between items-center mb-4">
+			<h1 className="text-2xl">StreakUp</h1>
+			<Link
+				className="border border-slate-300 text-slate-300 rounded-md px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+				href="/new">New Habit</Link>
+			</header>
+			<ul>
+				{habits.map(h=><li key={h.id}>{h.name}</li>)}
+			</ul>
+		</>
+	)
 }
