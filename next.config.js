9d5688d6bd9e296a7f7a82333299a00654dbb012
Reza Zahedi <r.zahedi@gmail.com>
2024-01-16 00:31:58 -0800

    update: nextjs updated to latest

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true,
-	},
+	experimental: {},
   images: {
     domains: [
       "lh3.googleusercontent.com",
