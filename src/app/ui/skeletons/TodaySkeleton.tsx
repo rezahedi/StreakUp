c1f481a3cbbee42524a0deac87d0994542614b48
Reza Zahedi <r.zahedi@gmail.com>
2024-01-19 14:31:55 -0800

    fix: skeleton display issue

--- src/app/ui/skeletons/TodaySkeleton.tsx
+++ src/app/ui/skeletons/TodaySkeleton.tsx
@@ -1,19 +1,18 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function TodaySkeleton()
 {
   return (
-    <SkeletonTheme baseColor="#202020" highlightColor="#444444">
       <div className="flex gap-4 w-full">
         <div className="w-16">
-          <Skeleton circle width={64} />
+          <Skeleton circle height={64} />
         </div>
         <div className="grow">
-          <Skeleton count={1} height={30} width={50} />
-          <Skeleton count={1} height={20} width={150} />
-          <Skeleton count={1} height={20} width={100} />
+          <Skeleton count={1} height={30} width={150} />
+          <Skeleton count={1} height={20} width={250} />
+          <Skeleton count={1} height={20} width={200} />
         </div>
       </div>
-    </SkeletonTheme>
   );
 }
