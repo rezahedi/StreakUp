e46ef8a57790896ecc65dfd05a931cd1931c53c8
Reza Zahedi <r.zahedi@gmail.com>
2024-01-19 23:29:20 -0800

    image priority and size changed to improve LCP

--- src/app/ui/NoHabits.tsx
+++ src/app/ui/NoHabits.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function NoHabits() {
   return (
     <div className="flex flex-col items-center text-green-500 py-10">
-      <Image src="/images/allset.gif" alt='all set' className='rounded-xl mb-2' width={300} height={300} priority={false} />
+      <Image src="/images/allset.gif" alt='all set' className='rounded-xl mb-2 w-auto h-auto' width={450} height={250} priority />
       <p>You are all set for now!</p>
     </div>
   )
