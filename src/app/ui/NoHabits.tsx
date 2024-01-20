bc1000757eec49e8f3426935c643cf0c22647f02
Reza Zahedi <r.zahedi@gmail.com>
2024-01-19 23:11:12 -0800

    image priority changes to improve LCP

--- src/app/ui/NoHabits.tsx
+++ src/app/ui/NoHabits.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function NoHabits() {
   return (
     <div className="flex flex-col items-center text-green-500 py-10">
-      <Image src="/images/allset.gif" alt='all set' className='rounded-xl mb-2' width={300} height={300} />
+      <Image src="/images/allset.gif" alt='all set' className='rounded-xl mb-2' width={300} height={300} priority={false} />
       <p>You are all set for now!</p>
     </div>
   )
--- src/app/ui/Welcome.tsx
+++ src/app/ui/Welcome.tsx
@@ -4,7 +4,7 @@ import CreateHabitBtn from "./CreateHabitBtn";
 export default function Welcome() {
   return (
     <div className="flex flex-col items-center text-green-500 py-16">
-      <Image src="/images/welcome.gif" alt='welcome' className='rounded-xl mb-2' width={450} height={250} />
+      <Image src="/images/welcome.gif" alt='welcome' className='rounded-xl mb-2' width={450} height={250} priority={false} />
       <p>Create your first habit to get started!</p>
       <CreateHabitBtn text="First Habit" className="mt-6" />
     </div>
