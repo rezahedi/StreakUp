8fd707470632b98157bff4180abde0bbc78e2da9
Reza Zahedi <r.zahedi@gmail.com>
2024-01-19 20:08:52 -0800

    fetch all habits limited to 20

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -84,6 +84,7 @@ export const fetchBrokenData = async () =>
 	})
 }
 
+// Fetch all habits but limited to 20
 export const fetchAllData = async () =>
 {
 	await new Promise(resolve => setTimeout(resolve, 1000));
@@ -102,6 +103,7 @@ export const fetchAllData = async () =>
 		},
 		orderBy: {
 			startDate: 'desc'
-		}
+		},
+		take: 20
 	})
 }
