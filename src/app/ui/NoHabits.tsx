a0548b42cb750d731e9aa6050d491be540ef72b4
Reza Zahedi <r.zahedi@gmail.com>
2024-01-19 20:09:43 -0800

    UI: gif images added

--- src/app/ui/NoHabits.tsx
+++ src/app/ui/NoHabits.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image'
 
 export default function NoHabits() {
   return (
-    <div className="text-center text-green-500 py-16">You are all set for now!</div>
+    <div className="flex flex-col items-center text-green-500 py-10">
+      <Image src="/images/allset.gif" alt='all set' className='rounded-xl mb-2' width={300} height={300} />
+      <p>You are all set for now!</p>
+    </div>
   )
 }
--- src/app/ui/Welcome.tsx
+++ src/app/ui/Welcome.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image'
 import CreateHabitBtn from "./CreateHabitBtn";
 
 export default function Welcome() {
   return (
     <div className="flex flex-col items-center text-green-500 py-16">
-      <b>Welcome to StreakUp 🎉</b>
+      <Image src="/images/welcome.gif" alt='welcome' className='rounded-xl mb-2' width={450} height={250} />
       <p>Create your first habit to get started!</p>
-      <CreateHabitBtn text="First Habit" className="mt-4" />
+      <CreateHabitBtn text="First Habit" className="mt-6" />
     </div>
   )
 }
