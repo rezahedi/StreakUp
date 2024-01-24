16c7a73de0ee63b403896a65b4907bdfbbca7916
Reza Zahedi <r.zahedi@gmail.com>
2024-01-24 11:51:51 -0800

    Sidebar: link to home

--- src/app/ui/Sidebar.tsx
+++ src/app/ui/Sidebar.tsx
@@ -5,6 +5,7 @@ export default function Sidebar() {
     <div className="scrollbar-hide sticky top-20 col-span-2 mt-6 hidden max-h-[calc(100vh-150px)] self-start overflow-auto rounded-lg border border-gray-100 bg-white shadow lg:block">
       <div className="grid w-full rounded-md bg-white p-6 lg:divide-y lg:divide-gray-300">
         <ul>
+          <li><Link href="./">Today Habits</Link></li>
           <li><Link href="./broken">Streak Broken Habits</Link></li>
           <li><Link href="./finished">Reached Goal Habits</Link></li>
         </ul>
