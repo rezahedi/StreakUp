b845df4d9c27edf9612abc11becd430a61f340eb
Reza Zahedi <r.zahedi@gmail.com>
2024-01-23 23:12:17 -0800

    mobile UI improved

--- src/app/ui/BrokenItem.tsx
+++ src/app/ui/BrokenItem.tsx
@@ -23,26 +23,28 @@ export default function BrokenItem(
 	}
 
   return (
-		<li className="flex gap-4 items-center rounded-lg bg-slate-100 my-4 p-4" role="listitem">
+		<li className="flex gap-4 items-center rounded-lg bg-slate-100 my-4 sm:p-4 p-2" role="listitem">
 			<div className='aspect-square'>
 				<ProgressBar size={90} progress={0} label={`${goal} days`} />
 			</div>
-			<div className="flex flex-col grow">
-				<span className="text-slate-400 text-xs">{patternObject.readablePattern}</span>
-				<div className={`text-lg text-slate-500 py-2`+(!active && ` line-through`)}>{emoji} {name}</div>
-				<div className='flex gap-8'>
-					<span className="flex gap-1 text-slate-400 text-xs">
-						<FontAwesomeIcon icon={faCalendarCheck} className='w-3' />
-						{lastStreak ? `${lastStreak}d best streak` : `Never started`}</span>
+			<div className="grow flex gap-4 sm:items-center flex-col sm:flex-row">
+				<div className="flex flex-col grow">
+					<span className="text-slate-400 text-xs">{patternObject.readablePattern}</span>
+					<div className={`text-lg text-slate-500 py-2`+(!active && ` line-through`)}>{emoji} {name}</div>
+					<div className='flex gap-8'>
+						<span className="flex gap-1 text-slate-400 text-xs">
+							<FontAwesomeIcon icon={faCalendarCheck} className='w-3' />
+							{lastStreak ? `${lastStreak}d best streak` : `Never started`}</span>
+					</div>
 				</div>
+				{active && <span className='text-green-500'>Activated!</span>}
+				{!active && 
+					<button
+						onClick={e => handleClick(id)}
+						className='self-end sm:self-center border text-orange-500 border-orange-500 rounded px-2 py-1 hover:bg-orange-500 hover:bg-opacity-20 focus-within:bg-orange-500 focus-within:bg-opacity-20 whitespace-nowrap'>
+						Activate</button>
+				}
 			</div>
-			{active && <span className='text-green-500'>Activated!</span>}
-			{!active && 
-				<button
-					onClick={e => handleClick(id)}
-					className='border text-orange-500 border-orange-500 rounded px-2 py-1 hover:bg-orange-500 hover:bg-opacity-20 focus-within:bg-orange-500 focus-within:bg-opacity-20 whitespace-nowrap'>
-					Activate</button>
-			}
 		</li>
   )
 }
--- src/app/ui/TodayItem.tsx
+++ src/app/ui/TodayItem.tsx
@@ -23,31 +23,33 @@ export default function TodayItem(
 	}
 
   return (
-		<li className="flex gap-4 items-center rounded-lg bg-slate-100 my-4 p-4" role="listitem">
+		<li className="flex gap-4 items-center rounded-lg bg-slate-100 my-4 sm:p-4 p-4" role="listitem">
 			<div className='aspect-square'>
 				<ProgressBar size={90} progress={(100/goal)*streak} label={`${streak}/${goal} days`} />
 			</div>
-			<div className="flex flex-col grow">
-				<span className="text-slate-400 text-xs">{patternObject.readablePattern}</span>
-				<div className={`text-lg py-2`+(checkin && ` line-through`)}>{emoji} {name}</div>
-				<div className='flex gap-8'>
-					<span className="flex gap-1 text-slate-400 text-xs">
-						<FontAwesomeIcon icon={faCalendarCheck} className='w-3' />
-						{streak ? `${streak}d streak / ` : ``}
-						{lastStreak ? `${lastStreak}d best streak` : `Not started`}
-					</span>
-					<time className="flex gap-1 text-slate-400 text-xs">
-						<FontAwesomeIcon icon={faCalendarDays} className='w-3' />
-						{createdAt.toLocaleDateString()}</time>
+			<div className="grow flex gap-4 sm:items-center flex-col sm:flex-row">
+				<div className="flex flex-col grow">
+					<span className="text-slate-400 text-xs">{patternObject.readablePattern}</span>
+					<div className={`text-lg py-2`+(checkin && ` line-through`)}>{emoji} {name}</div>
+					<div className='flex gap-8'>
+						<span className="flex gap-1 text-slate-400 text-xs">
+							<FontAwesomeIcon icon={faCalendarCheck} className='w-3' />
+							{streak ? `${streak}d streak / ` : ``}
+							{lastStreak ? `${lastStreak}d best streak` : `Not started`}
+						</span>
+						<time className="hidden sm:flex gap-1 text-slate-400 text-xs">
+							<FontAwesomeIcon icon={faCalendarDays} className='w-3' />
+							{createdAt.toLocaleDateString()}</time>
+					</div>
 				</div>
+				{checkin && <span className='text-green-500'>Checked in</span>}
+				{!checkin && 
+					<button
+						onClick={e => handleClick(id)}
+						className='self-end sm:self-center border text-orange-500 border-orange-500 rounded px-2 py-1 hover:bg-orange-500 hover:bg-opacity-20 focus-within:bg-orange-500 focus-within:bg-opacity-20 whitespace-nowrap'>
+						Check-in</button>
+				}
 			</div>
-			{checkin && <span className='text-green-500'>Checked in</span>}
-			{!checkin && 
-				<button
-					onClick={e => handleClick(id)}
-					className='border text-orange-500 border-orange-500 rounded px-2 py-1 hover:bg-orange-500 hover:bg-opacity-20 focus-within:bg-orange-500 focus-within:bg-opacity-20 whitespace-nowrap'>
-					Check-in</button>
-			}
 		</li>
   )
 }
