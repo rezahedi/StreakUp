309d9f3e625774392ee37920e366eea290cf2626
Reza Zahedi <r.zahedi@gmail.com>
2024-01-24 15:22:05 -0800

    timeout remoed from fetch

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -7,8 +7,6 @@ import { authOptions } from '@/app/lib/auth';
 // Fetch all habits but limited to 20
 export const fetchAllData = async () =>
 {
-	await new Promise(resolve => setTimeout(resolve, 1000));
-
 	// Check if user is logged in
 	const session = await getServerSession(authOptions);
 	if (!session || !session.user)
