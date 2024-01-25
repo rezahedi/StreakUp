c30183470734b53a2608658152f2a71f44082875
Reza Zahedi <r.zahedi@gmail.com>
2024-01-25 00:23:25 -0800

    fix: habit item context menu errors

--- src/app/ui/ContextMenuButton.tsx
+++ src/app/ui/ContextMenuButton.tsx
@@ -6,21 +6,21 @@ export default function ContextMenuButton({ id, remove }: { id: string, remove: 
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <button type="button" className="sm:inline-flex rounded-md px-1 py-2 transition-all duration-75 hover:bg-gray-100 active:bg-gray-200" aria-haspopup="dialog" aria-expanded="true" aria-controls="radix-:rcv:" data-state="open">
+        <div className="sm:inline-flex rounded-md px-1 py-2 transition-all duration-75 hover:bg-gray-100 active:bg-gray-200" aria-haspopup="dialog" aria-expanded="true" aria-controls="radix-:rcv:" data-state="open">
           <span className="sr-only">Edit</span>
-          <svg fill="none" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="14" height="14" className="h-5 w-5 text-gray-500">
+          <svg fill="none" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="14" height="14" className="h-5 w-5 text-gray-500">
             <circle cx="12" cy="12" r="1"></circle>
             <circle cx="12" cy="5" r="1"></circle>
             <circle cx="12" cy="19" r="1"></circle>
           </svg>
-        </button>
+        </div>
       </DropdownMenu.Trigger>
     <DropdownMenu.Content align="end" className="w-60 p-4 animate-fade-in rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-      <DropdownMenu.Item disabled className="block cursor-pointer p-2 text-sm text-gray-700 hover:bg-gray-100"
+      <DropdownMenu.Item className="block cursor-pointer p-2 text-sm text-gray-700 hover:bg-gray-100"
         onSelect={() => {}}>
         Show Details
       </DropdownMenu.Item>
-      <DropdownMenu.Item disabled className="block cursor-pointer p-2 text-sm text-gray-700 hover:bg-gray-100"
+      <DropdownMenu.Item className="block cursor-pointer p-2 text-sm text-gray-700 hover:bg-gray-100"
         onSelect={() => {}}>
         Edit
       </DropdownMenu.Item>
