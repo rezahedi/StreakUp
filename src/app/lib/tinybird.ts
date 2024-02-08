75d62ad50595d72f381230f5d33ea6227b7ebe77
Reza Zahedi <r.zahedi@gmail.com>
2024-02-07 18:12:37 -0800

    feat: Data fetched from Tinybird

--- src/app/lib/tinybird.ts
+++ src/app/lib/tinybird.ts
@@ -14,3 +14,26 @@ export async function recordEvent(key: string) {
     body: JSON.stringify(payload)
   }).then((res) => res.json())
 }
+
+export async function fetchCounts() {
+  let result = await fetch('https://api.us-east.tinybird.co/v0/pipes/counts.json', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.TINYBIRD_API_KEY
+    }
+  })
+  .then((res) => res.json())
+  .then(r => r)
+  .catch(e => e.toString())
+
+  if (!result.data)
+    return null
+
+  // Convert the array of objects
+  // from [{ "key": "create", "count": 1 }]
+  // to [{ "create": 1 }]
+  return await result.data.reduce((prev: any, curr: any) => {
+    return { ...prev, [curr.key]: curr.count }
+  }, {})
+}
--- src/components/home/Stats.tsx
+++ src/components/home/Stats.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import { fetchCounts } from '@/app/lib/tinybird';
 
-export default function Stats() {
+export default async function Stats() {
+  const stats = await fetchCounts();
+
   return (
     <div className="border-y border-gray-200 bg-white/10 my-12 py-8 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur">
       <div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 grid gap-y-4 divide-x divide-gray-200 md:grid-cols-3 md:gap-y-0">
         <div className="flex flex-col items-center justify-center space-y-2">
-          <p className="text-4xl font-bold md:text-6xl">1,775</p>
+          <p className="text-4xl font-bold md:text-6xl">{(stats.signup || 0).toLocaleString()}</p>
           <p className="font-semibold uppercase text-gray-500 md:text-lg">Users Registered</p>
         </div>
         <div className="flex flex-col items-center justify-center space-y-2">
-          <p className="text-4xl font-bold md:text-6xl">334.2K</p>
+          <p className="text-4xl font-bold md:text-6xl">{(stats.create || 0).toLocaleString()}</p>
           <p className="font-semibold uppercase text-gray-500 md:text-lg">Habits Created</p>
         </div>
         <div className="flex flex-col items-center justify-center space-y-2">
-          <p className="text-4xl font-bold md:text-6xl">17.7M</p>
+          <p className="text-4xl font-bold md:text-6xl">{(stats.checkin || 0).toLocaleString()}</p>
           <p className="font-semibold uppercase text-gray-500 md:text-lg">Days Checked In</p>
         </div>
       </div>
